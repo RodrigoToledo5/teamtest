2107872b615739e25fe05873a2d6ef6bbdd8cb38
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-05 18:33:47 -0300

    change p

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Soy un cambio hecho desde el branch Fran >)
+          Soy un cambio hecho desde el branch Fran
         </p>
         <a
           className="App-link"
